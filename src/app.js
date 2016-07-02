ce806e559da1af3411326ee9bdaca52cac687040
John Doe <webdevnorth@gmail.com>
2016-07-02 15:20:24 +0100

    added .gitignore, procfile, updated package.json, changed port number in app.js

--- src/app.js
+++ src/app.js
@@ -100,6 +100,6 @@ app.get('/:time?', function (req, res) {
     }
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Server is running...');
 });
