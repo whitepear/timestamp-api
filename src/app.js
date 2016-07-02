2a76186c039f77e012fcb2b6e3b2aa1e1d7ecc6d
John Doe <webdevnorth@gmail.com>
2016-07-02 15:37:59 +0100

    removed redundant callback on app.listen

--- src/app.js
+++ src/app.js
@@ -100,6 +100,4 @@ app.get('/:time?', function (req, res) {
     }
 });
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Server is running...');
-});
+app.listen(process.env.PORT || 3000, console.log('Server is running...'));
